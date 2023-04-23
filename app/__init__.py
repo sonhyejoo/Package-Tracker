7ad3ab28cde85afc2ee11f4ad608884de1016efe
sonhyejoo <seongwoo.cho@gmail.com>
2023-04-23 19:19:47 -0400

    Add shipping request template, form, route

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
+from .shipping_form import ShippingForm
 from .config import Config
 
 app = Flask(__name__)
@@ -12,4 +13,7 @@ def index():
 
 @app.route("/new_package", methods=["GET", "POST"])
 def new_package():
-    pass
+    form = ShippingForm()
+    if form.validate_on_submit():
+        return redirect(url_for(".index"))
+    return render_template("shipping_request.html", form=form)
--- app/shipping_form.py
+++ app/shipping_form.py
@@ -1,12 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
+from app.maps import maps
+
+cities = list(maps.keys())
 
 
 class ShippingForm(FlaskForm):
     sender = StringField("sender", validators=[DataRequired()])
     recipient = StringField("recipient", validators=[DataRequired()])
-    origin = SelectField("origin", validators=[DataRequired()])
-    destination = SelectField("destination", validators=[DataRequired()])
-    express = BooleanField("express")
+    origin = SelectField("origin", choices=cities, validators=[DataRequired()])
+    destination = SelectField(
+        "destination", choices=cities, validators=[DataRequired()]
+    )
+    express = BooleanField("express", validators=[DataRequired()])
     submit = SubmitField("Submit")
